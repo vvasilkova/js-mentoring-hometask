1592726c54d2ae24587e43746b812c3528feea6e
Volha Vasilkova <Volha_Vasilkova@epam.com>
2020-07-02 12:16:33 +0300

    commit_task4_version2

--- tasks/functions/functions.js
+++ tasks/functions/functions.js
@@ -10,13 +10,6 @@ console.log(add(1, 2));
 console.log(add(-1, 2));
 console.log(add(-1, -2));
 
-//VAR2
-function add(a, b) {
-	return a + b;
-}
-let sum = add(1, 2)
-console.log(sum);
-
 
 /**
  * write function that return first and last name of given object
@@ -36,13 +29,6 @@ function getFullName(object) {
 	return object.firstName + " " + object.lastName;
 }
 getFullName(object);
-
-//OR
-function getFullName(firstName, lastName) {
-	return firstName + " " + lastName;
-}
-getFullName('John', 'Dou');
-
 
 /**
  * write fuction that checks is number is odd
@@ -64,19 +50,7 @@ isOdd(n);
  * write function that return shortest of words in given array
  * e.g ["one", "two", "three"] should return one
  */
-let wordArray = ["one", "two", "three"];
 
-function getShortest(wordArray) {
-	for (let i = 0; i < wordArray.length; i++) {
-		if (wordArray[i] < wordArray[0]) {
-			wordArray[0] = wordArray[i]
-		};
-	}
-	return wordArray[0];
-}
-console.log(getShortest(wordArray));
-
-//FROM Meeting:
 function getShortest(wordArray) {
 	let min = wordArray[0];
 	for (let i = 1; i < wordArray.length; i++) {
@@ -86,20 +60,19 @@ function getShortest(wordArray) {
 	}
 	return min;
 }
-console.log(getShortest(wordArray));
+
 
 
 /**
  * write function that returns word google with given numbers of "o" symbols
  * e.g getGoogle(5) should return "gooooogle"
  */
-let n = 6
+
 
 function getGoogle(n) {
-	let x = 'o'.repeat(n);
-	return 'g' + x + 'gle';
+	return 'g' + 'o'.repeat(n) + 'gle';
 }
-console.log(getGoogle(n))
+console.log(getGoogle(n));
 
 
 /**
@@ -143,17 +116,6 @@ function getTotalPath(path) {
 }
 console.log(getTotalPath(path))
 
-//OR
-function getTotalPath(path) {
-	let totalPath = 0;
-	for (let x of path) {
-		totalPath += x.distance;
-	}
-	return totalPath;
-}
-console.log(getTotalPath(path))
-
-
 /**
  * write a function that will calculate a discount considering the Amount
  * and the percentage (hint: you need to use the Closure here)
@@ -162,20 +124,13 @@ console.log(getTotalPath(path))
  */
 
 function discountFunction(percentage) {
-	return function (amount) {
+	let amount = 90;
+		return function (amount) {
 		return amount - (amount * percentage) / 100;
 	};
 }
-
-const discount10 = discountFunction(10);
-console.log(discount10(90));
-
-const discount15 = discountFunction(15);
-console.log(discount15(110));
-
-const discount20 = discountFunction(20);
-console.log(discount20(100));
-
+let discountfunction10 = discountFunction(10)
+console.log(discountFunction(10))
 
 /**
  * Write the methods inside the given objects that:
@@ -190,14 +145,17 @@ const myObject = {
 	age: 25,
 	friends: ['Mike', 'Alan', 'Daniel'],
 	keys() {
-		console.log(Object.keys(myObject));
+		for (let key in myObject) {
+			console.log(key);
+		}
+
 	},
 	call() {
-		return 'My name is ' + myObject.name + " " + myObject.lastName + 'and I am ' + myObject.age + ' years old. My best friend is ' + myObject.friends[2]
+		return 'My name is ' + myObject.name + " " + myObject.lastName + " " +  'and I am ' + myObject.age + ' years old. My best friend is ' + myObject.friends[2]
 	}
 };
 
-console.log(myObject.keys())
+console.log(myObject.keys());
 console.log(myObject.call())
 
 
